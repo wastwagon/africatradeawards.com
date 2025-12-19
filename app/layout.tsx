8e11536e1822f4b7dfcbdea90ca3a9785188863e
wastwagon <gilbert.amidu@gmail.com>
2025-12-19 07:52:30 +0000

    Fix FOUC with blocking script to load CSS before React hydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import './globals.css'
 
 import type { Metadata } from "next"
 import { Figtree, Space_Grotesk } from "next/font/google"
-import Script from 'next/script'
 
 const figtree = Figtree({
 	weight: ['300', '400', '500', '600', '700', '800', '900'],
@@ -33,22 +32,35 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				{/* Preload critical CSS files to prevent FOUC */}
-				<link rel="preload" href="/assets/css/vendor/bootstrap.min.css" as="style" />
-				<link rel="preload" href="/assets/css/vendor/fontawesome.css" as="style" />
-				<link rel="preload" href="/assets/css/main.css" as="style" />
-				
-				{/* Load CSS synchronously in head to prevent FOUC */}
-				<link rel="stylesheet" href="/assets/css/vendor/bootstrap.min.css" />
-				<link rel="stylesheet" href="/assets/css/vendor/fontawesome.css" />
-				<link rel="stylesheet" href="/assets/css/main.css" />
-				<link rel="stylesheet" href="/assets/css/vendor/aos.css" />
-				<link rel="stylesheet" href="/assets/css/vendor/magnific-popup.css" />
-				<link rel="stylesheet" href="/assets/css/vendor/mobile.css" />
-				<link rel="stylesheet" href="/assets/css/vendor/sidebar.css" />
-				<link rel="stylesheet" href="/assets/css/vendor/slick-slider.css" />
-				<link rel="stylesheet" href="/assets/css/vendor/nice-select.css" />
-				<link rel="stylesheet" href="/assets/css/vendor/odometer.css" />
+				{/* Blocking script to load CSS before React hydrates - prevents FOUC */}
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function() {
+								const cssFiles = [
+									'/assets/css/vendor/bootstrap.min.css',
+									'/assets/css/vendor/fontawesome.css',
+									'/assets/css/main.css',
+									'/assets/css/vendor/aos.css',
+									'/assets/css/vendor/magnific-popup.css',
+									'/assets/css/vendor/mobile.css',
+									'/assets/css/vendor/sidebar.css',
+									'/assets/css/vendor/slick-slider.css',
+									'/assets/css/vendor/nice-select.css',
+									'/assets/css/vendor/odometer.css'
+								];
+								cssFiles.forEach(function(href) {
+									if (!document.querySelector('link[href="' + href + '"]')) {
+										const link = document.createElement('link');
+										link.rel = 'stylesheet';
+										link.href = href;
+										document.head.appendChild(link);
+									}
+								});
+							})();
+						`,
+					}}
+				/>
 			</head>
 			<body className={`${figtree.variable} ${grotesk.variable} homepage1-body`}>
 				{children}
