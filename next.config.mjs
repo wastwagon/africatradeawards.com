9a23075acf5335bfc33e1a60ccb30110d141167b
wastwagon <gilbert.amidu@gmail.com>
2026-01-09 19:03:29 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'standalone',
+	output: 'export',
 	images: {
-		unoptimized: true,
+		unoptimized: true, // Required for static export
 	},
 	trailingSlash: true,
 };
