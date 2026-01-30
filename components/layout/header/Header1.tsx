fb0fffaba8b7f20d64735ca800aacaec14d8d32a
wastwagon <gilbert.amidu@gmail.com>
2026-01-30 13:06:21 +0000

    commit

--- components/layout/header/Header1.tsx
+++ components/layout/header/Header1.tsx
@@ -38,6 +38,9 @@ export default function Header1({ scroll, isMobileMenu, handleMobileMenu, isSear
 											<li className={isActive('/awards-structure') ? 'active' : ''}>
 												<Link href="/awards-structure">Awards Structure</Link>
 											</li>
+											<li className={isActive('/awardees') ? 'active' : ''}>
+												<Link href="/awardees">Awardees</Link>
+											</li>
 											<li className={isActive('/sponsors-partners') ? 'active' : ''}>
 												<Link href="/sponsors-partners">Sponsors & Partners</Link>
 											</li>
